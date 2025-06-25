39fcc988f9d207a3c20e717b93eb8582c950bf00
pcasaretto <paulo.casaretto@shopify.com>
2025-06-25 14:41:11 +0200

    move global darwin config to home-manager

--- home-manager/darwin/default.nix
+++ home-manager/darwin/default.nix
@@ -2,4 +2,35 @@
 {
   imports = [
   ];
+
+  services.macos-remap-keys = {
+   enable = true;
+   keyboard.Capslock = "Control";
+  };
+
+  targets.darwin.defaults = {
+    # Finder preferences
+    "com.apple.finder" = {
+      FXPreferredViewStyle = "Nlsv";
+      AppleShowAllFiles = true;
+      AppleShowAllExtensions = true;
+    };
+    NSGlobalDomain = {
+      # do not use press and hold for special characters
+      ApplePressAndHoldEnabled = false;
+      KeyRepeat = 2;
+      InitialKeyRepeat = 15;
+    };
+
+    "com.apple.AppleMultitouchTrackpad" = {
+      Clicking = true;
+      TrackpadThreeFingerDrag = true;
+    };
+
+    "com.apple.dock" = {
+      autohide = true;
+      show-recents = false;
+    };
+  };
+
 }
--- hosts/common/darwin/default.nix
+++ hosts/common/darwin/default.nix
@@ -2,35 +2,7 @@
 {
   imports = [
   ];
-  # Keyboard
-
-  # Keyboard
-  system.keyboard.enableKeyMapping = true;
-  system.keyboard.remapCapsLockToControl = true;
 
   # use TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
-
-  system.defaults = {
-    # Finder preferences
-    finder = {
-      FXPreferredViewStyle = "Nlsv";
-      AppleShowAllFiles = true;
-      AppleShowAllExtensions = true;
-    };
-    NSGlobalDomain = {
-      # do not use press and hold for special characters
-      ApplePressAndHoldEnabled = false;
-      KeyRepeat = 2;
-      InitialKeyRepeat = 15;
-    };
-
-    trackpad.Clicking = true;
-    trackpad.TrackpadThreeFingerDrag = true;
-
-    dock = {
-      autohide = true;
-      show-recents = false;
-    };
-  };
 }
